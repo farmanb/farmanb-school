c8cb90a4341e7fa7df38af593fd03ba3f17564ca
farmanb <farmanb@bddb60e6-0788-11de-a6e9-d320b29efae0>
2009-03-04 20:07:22 +0000

    Attempt2

---
+++ trunk/roboticsII/hw2/partC/App.h
@@ -0,0 +1,27 @@
+#include "AppGL.h"
+class BallController;
+class GripControl;
+
+class App : public DVC_AppGL{
+ public:
+  App();
+  ~App();
+  
+  bool Init();
+  void PostStep();
+
+ private:
+  bool GetBodies();
+  bool AddControllers();
+  void GoToPartCG();
+  
+  std::string **gripName,
+    *partName, *palmName,
+    *ballName;
+
+  DynamicalBody *part, **grip,
+    *palm, *ball;
+  GripControl **gripController;
+  BallController *ballController;
+  bool shot;
+};
